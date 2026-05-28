3c3c0d3ac452e519ce3a51cfa3463b336d18d094
Daud Sulaimon <owologeson1@gmail.com>
2026-05-28 18:45:34 +0100

    Revise project details in Project.jsx
    Updated project titles and descriptions for clarity.

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -14,13 +14,12 @@ export const Projects = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <h3 className="text-xl font-semibold mb-2">Cloud Platform</h3>
+            <h3 className="text-xl font-semibold mb-2">Solarproject</h3>
             <p className="text-gray-400 mb-4">
-              Scalable cloud infrastructure management with real-time monitoring
-              and automated scaling.
+              Revolutionary Home Solar System.Solar system that focusig on turning your vision into reality by focusing on the basic.
             </p>
             <div className="flex flex-wrap gap-2">
-              {["React", "TailwindCSS"].map((tech, key) => (
+              {["React", "CSS"].map((tech, key) => (
                 <span
                   key={key}
                   className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all"
@@ -35,10 +34,9 @@ export const Projects = () => {
           </div>
 
          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-            <h3 className="text-xl font-semibold mb-2">Cloud Platform</h3>
+            <h3 className="text-xl font-semibold mb-2">Orebi Eccomerce </h3>
             <p className="text-gray-400 mb-4">
-              Scalable cloud infrastructure management with real-time monitoring
-              and automated scaling.
+             Orebi commerce is a e-commerce web application built for retailer, secure user authentication and seamlesss checkout .
             </p>
             <div className="flex flex-wrap gap-2">
               {["React", "TailwindCSS"].map((tech, key) => (
